8ea59a6ba6595a00745a7e617603b6e20c31e719
Malikom12 <muhammadmalikomer@gmail.com>
2024-11-06 14:15:34 +0000

    Initial commit

---
+++ CommentsFilter/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace CommentsFilter;
+
+public class Comment
+{
+    private string RawComment { get; set; }
+    private string SanitizedComment { get; set; }
+
+    public Comment(string rawComment, string sanitizedComment)
+    {
+        RawComment = rawComment;
+        sanitizedComment = sanitizedComment;
+    }
+}
